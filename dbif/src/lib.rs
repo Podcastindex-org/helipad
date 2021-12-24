b370feac42f539021d2bf3a968a2bac9f52cd1c9
Martin Habovstiak <martin.habovstiak@gmail.com>
2021-12-24 11:32:23 +0100

    Configuration cleanup
    This change makes `configure_me` do the job of reading environment
    variables, significantly reducing the required code. It also changes use
    of `String` to `PathBuf` where appropriate and removes some string
    manipulations.

--- dbif/src/lib.rs
+++ dbif/src/lib.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use std::fmt;
 use serde::{Deserialize, Serialize};
 use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
 
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -31,26 +32,26 @@ impl fmt::Display for HydraError {
 impl Error for HydraError {}
 
 
-fn connect_to_database(init: bool, filepath: &String) -> Result<Connection, Box<dyn Error>> {
-    if let Ok(conn) = Connection::open(filepath.as_str()) {
+fn connect_to_database(init: bool, filepath: &Path) -> Result<Connection, Box<dyn Error>> {
+    if let Ok(conn) = Connection::open(filepath) {
         if init {
-            match set_database_file_permissions(filepath.as_str()) {
+            match set_database_file_permissions(filepath) {
                 Ok(_) => {},
                 Err(e) => {
                     eprintln!("{:#?}", e);
                 }
             }
-            println!("Using database file: [{}]", filepath.as_str());
+            println!("Using database file: [{}]", filepath.display());
         }
         Ok(conn)
     } else {
-        return Err(Box::new(HydraError(format!("Could not open a database file at: [{}].", filepath).into())))
+        return Err(Box::new(HydraError(format!("Could not open a database file at: [{}].", filepath.display()).into())))
     }
 }
 
 
 //Set permissions on the database file
-fn set_database_file_permissions(filepath: &str) -> Result<bool, Box<dyn Error>> {
+fn set_database_file_permissions(filepath: &Path) -> Result<bool, Box<dyn Error>> {
 
     match std::fs::File::open(filepath) {
         Ok(fh) => {
@@ -58,23 +59,23 @@ fn set_database_file_permissions(filepath: &str) -> Result<bool, Box<dyn Error>>
                 Ok(metadata) => {
                     let mut perms = metadata.permissions();
                     perms.set_mode(0o666);
-                    println!("Set file permission to: [666] on database file: [{}]", filepath);
+                    println!("Set file permission to: [666] on database file: [{}]", filepath.display());
                     Ok(true)
                 },
                 Err(e) => {
-                    return Err(Box::new(HydraError(format!("Error getting metadata from database file handle: [{}].  Error: {:#?}.", filepath, e).into())))
+                    return Err(Box::new(HydraError(format!("Error getting metadata from database file handle: [{}].  Error: {:#?}.", filepath.display(), e).into())))
                 }
             }
         },
         Err(e) => {
-            return Err(Box::new(HydraError(format!("Error opening database file handle: [{}] for permissions setting.  Error: {:#?}.", filepath, e).into())))
+            return Err(Box::new(HydraError(format!("Error opening database file handle: [{}] for permissions setting.  Error: {:#?}.", filepath.display(), e).into())))
         }
     }
 }
 
 
 //Create a new database file if needed
-pub fn create_database(filepath: &String) -> Result<bool, Box<dyn Error>> {
+pub fn create_database(filepath: &Path) -> Result<bool, Box<dyn Error>> {
     let conn = connect_to_database(true, filepath)?;
 
     match conn.execute(
@@ -98,14 +99,14 @@ pub fn create_database(filepath: &String) -> Result<bool, Box<dyn Error>> {
         }
         Err(e) => {
             eprintln!("{}", e);
-            return Err(Box::new(HydraError(format!("Failed to create database: [{}].", filepath).into())))
+            return Err(Box::new(HydraError(format!("Failed to create database: [{}].", filepath.display()).into())))
         }
     }
 }
 
 
 //Add an invoice to the database
-pub fn add_invoice_to_db(filepath: &String, boost: BoostRecord) -> Result<bool, Box<dyn Error>> {
+pub fn add_invoice_to_db(filepath: &Path, boost: BoostRecord) -> Result<bool, Box<dyn Error>> {
     let conn = connect_to_database(false, filepath)?;
 
     match conn.execute("INSERT INTO boosts (idx, time, value_msat, value_msat_total, action, sender, app, message, podcast, episode, tlv) \
@@ -134,7 +135,7 @@ pub fn add_invoice_to_db(filepath: &String, boost: BoostRecord) -> Result<bool, 
 
 
 //Get all of the boosts from the database
-pub fn get_boosts_from_db(filepath: &String, index: u64, max: u64, direction: bool) -> Result<Vec<BoostRecord>, Box<dyn Error>> {
+pub fn get_boosts_from_db(filepath: &Path, index: u64, max: u64, direction: bool) -> Result<Vec<BoostRecord>, Box<dyn Error>> {
     let conn = connect_to_database(false, filepath)?;
     let mut boosts: Vec<BoostRecord> = Vec::new();
 
@@ -189,7 +190,7 @@ pub fn get_boosts_from_db(filepath: &String, index: u64, max: u64, direction: bo
 
 
 //Get the last boost index number from the database
-pub fn get_last_boost_index_from_db(filepath: &String) -> Result<u64, Box<dyn Error>> {
+pub fn get_last_boost_index_from_db(filepath: &Path) -> Result<u64, Box<dyn Error>> {
     let conn = connect_to_database(false, filepath)?;
     let mut boosts: Vec<BoostRecord> = Vec::new();
     let max = 1;
