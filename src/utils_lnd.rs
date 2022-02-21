a6a69c647a720b1aae085ac29584c164581579c2
Arno <arnobugs@hotmail.com>
2022-02-21 13:30:08 +0100

    fix lnd functions

--- src/utils_lnd.rs
+++ src/utils_lnd.rs
@@ -2,6 +2,13 @@ use std::error::Error;
 use std::fs;
 use lnd;
 
+const HELIPAD_CONFIG_FILE: &str = "./helipad.conf";
+const HELIPAD_DATABASE_DIR: &str = "database.db";
+const HELIPAD_STANDARD_PORT: &str = "2112";
+const LND_STANDARD_GRPC_URL: &str = "https://127.0.0.1:10009";
+const LND_STANDARD_MACAROON_LOCATION: &str = "/lnd/data/chain/bitcoin/mainnet/admin.macaroon";
+const LND_STANDARD_TLSCERT_LOCATION: &str = "/lnd/tls.cert";
+
 
 pub async fn get_macaroon(config_setting: String) -> Vec<u8> {
     //Get the macaroon file.  Look in the local directory first as an override.
