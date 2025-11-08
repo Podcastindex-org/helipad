6195057ca1975370c195f9773b7cd806f21898da
Eric P <ericpp@gmail.com>
2025-11-08 17:46:18 -0600

    Add separate setBalance function

--- webroot/script/balance.js
+++ webroot/script/balance.js
@@ -1,51 +1,57 @@
-$(document).ready(function () {
-    let currentBalance = null;
+const Balance = {
+    currentBalance: null,
 
-    //Get the current channel balance from the node
-    function getBalance() {
+    getBalance: function() {
         //Get the current boost index number
         $.ajax({
             url: "/api/v1/balance",
             type: "GET",
             contentType: "application/json; charset=utf-8",
             dataType: "json",
-            error: function (xhr) {
+            error: (xhr) => {
                 if (xhr.status === 403) {
                     window.location.href = "/login";
                 }
             },
-            success: function (balance) {
+            success: (balance) => {
                 // If the data returned wasn't a number then give an error
                 if (typeof balance !== "number") {
                     $('div.balanceDisplay').html('<span class="error" title="Error getting balance.">Err</span>');
                     return;
                 }
 
-                if (balance === currentBalance) {
-                    return; // no change
-                }
+                this.setBalance(balance);
+            },
 
-                // Display the balance
-                $('div.balanceDisplay').text('Balance: ').append(
-                    $('<span class="balance"></span>').text(numberFormat(balance))
-                );
-
-                // If the balance went up, do some fun stuff
-                if (currentBalance && balance > currentBalance) {
-                    $('div.balanceDisplay').addClass('bump').on('animationend', () => {
-                        $('div.balanceDisplay').removeClass('bump');
-                    });
-                }
-
-                // This is now the current balance
-                currentBalance = balance;
-            }
         });
-    }
+    },
 
-    getBalance();
+    setBalance: function(balance) {
+        if (balance === this.currentBalance) {
+            return; // no change
+        }
+
+        // Display the balance
+        $('div.balanceDisplay').text('Balance: ').append(
+            $('<span class="balance"></span>').text(numberFormat(balance))
+        );
+
+        // If the balance went up, do some fun stuff
+        if (this.currentBalance && balance > this.currentBalance) {
+            $('div.balanceDisplay').addClass('bump').on('animationend', () => {
+                $('div.balanceDisplay').removeClass('bump');
+            });
+        }
+
+        // This is now the current balance
+        this.currentBalance = balance;
+    }
+}
+
+$(document).ready(function () {
+    Balance.getBalance();
 
     setInterval(() => {
-        getBalance();
+        Balance.getBalance();
     }, 7000);
 });
